0bec55fb8e88fdcf03c8c933ec9354a5948c69bf
layzasilva <kermilynlay@gmail.com>
2024-10-09 10:20:32 -0300

    testando quebra de linhas utilizando for,lista e write

--- projeto.py/arquivos.py
+++ projeto.py/arquivos.py
@@ -1,4 +1,13 @@
 arq = open('arquivo.txt','w')
+
+# - write -
 # arq.write('olá mundo')
-lista = ['Layza','18','2006']
-arq.writelines(lista)
+
+# - writelines -
+# lista = ['Layza','18','2006']
+# arq.writelines(lista)
+
+# - quebra de linhas com write -
+nomes = ['Layza', 'Victor', 'Julia']
+for n in nomes:
+    arq.write(n + '\n')
